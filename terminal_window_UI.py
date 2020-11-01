fde011f6b1379e7d8eb5585891fc4b2eb4187e9b
Bryan Martinez <bryan-martinez-91@hotmail.com>
2020-11-01 10:19:13 +0100

    test 123

--- terminal_window_UI.py
+++ terminal_window_UI.py
@@ -1,11 +1,3 @@
 # -*- coding: utf-8 -*-
 import pandas as  pd
 import numpy as np
-from progress.bar import Bar
-
-bar = Bar('Processing', max=20)
-for i in range(20):
-    s=pd.array(np.random.randn(100000))
-    np.exp(s)
-    bar.next()
-bar.finish()
