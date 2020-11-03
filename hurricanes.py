b278b6dcc9c524ec1142ca31adc72abbfafeef63
Bryan Martinez <bryan-martinez-91@hotmail.com>
2020-11-03 23:33:50 +0100

    Hurricane plots working !

--- hurricanes.py
+++ hurricanes.py
@@ -2,7 +2,40 @@
 
 import numpy as np
 import pandas as pd
+import geopandas as gpd
 
-file = pd.read_csv('data/Hurricanes/Hurdat_Atlantic_Basin.csv', header=0)
 
-file.head(20)
+def coordinatetoStr(arg):
+    output = 0
+    arg.lstrip()
+    if 'N' in arg or 'E' in arg:
+        output = int(arg[:3])
+    elif 'S' in arg or 'W' in arg:
+        print(arg)
+        output = int(arg[:1])*-1
+    return output
+
+
+hurricanesfile = 'data/Hurricanes/Hurdat_Atlantic_Basin.csv'
+shapefile = 'data/Hurricanes/ne_110m_ocean.shp'
+columnnames2 = ['HurricaneID', 'HurricaneName', 'EntryNum']
+columnnames = list(range(0, 21))
+
+hurricaneCsv = pd.read_csv(hurricanesfile)
+mapObject = gpd.read_file(shapefile)
+
+# cleaning lists
+hurrDataframe = pd.DataFrame(hurricaneCsv)
+hurrDataframe.columns = columnnames
+hurrList = hurrDataframe.loc[hurrDataframe[0].str.contains('A')]
+hurrList = hurrList.drop(columns=list(range(3, 21)))
+hurrList.columns = columnnames2
+hurrDetails = hurrDataframe.loc[~hurrDataframe[0].str.contains('A')]
+
+coordinates = hurrDetails.drop(columns=list(range(0, 4)))
+coordinates = coordinates.drop(columns=list(range(6, 21)))
+
+coordinates.applymap(coordinatetoStr)
+
+mapObject.plot()
+.plot()
